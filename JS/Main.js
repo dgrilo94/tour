3cd09161f219f244dcdedc6b5bd42b3a981e01c6
dgrilo94 LAPTOP <58700087+dgrilo94@users.noreply.github.com>
2020-02-15 17:51:07 +0000

    UPD

--- JS/Main.js
+++ JS/Main.js
@@ -1,36 +1,3 @@
-// $(document).ready(function(){
-//   $('.slick').slick({
-//     infinite: true,
-//     speed: 300,
-//     slidesToShow: 1,
-//     arrows: false,
-//     autoplay: true,
-//     autoplaySpeed: 2000,
-//     dots: true,
-//     // dotsClass: '.dots',
-
-//   });
-//   });
-
-
-// $(document).ready(function () {
-
-//   var mySwiper = new Swiper('.swiper-container', {
-//     // Optional parameters
-//     direction: 'horizontal',
-//     loop: true,
-
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//       clickable: true,
-//       renderBullet: function (index, className) {
-//         return '<span class="' + className + '">' + (index + 1) + '</span>';
-//       },
-//     }
-//   });
-
-
   $( document ).ready(function() {
     //labels para os nomes das tours
     let labels = ['label1', 'label2', 'label3'];
@@ -50,7 +17,4 @@
         },
       },
     });
-
-
-
   });
